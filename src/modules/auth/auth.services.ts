fd4a8cb45fc4beb98c37aa32e7aeba6afd710956
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-24 03:08:40 +0600

    generated access secret and refresh secret through jwt

--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -1,23 +1,54 @@
+import config from "../../config/dotenv";
 import { prisma } from "../../lib/prisma";
 import { AuthPayLoad } from "./auth.interface";
 import bcrypt from "bcrypt"
+import jwt, { SignOptions } from "jsonwebtoken"
 
 
-const longinUser = async(payLoad : AuthPayLoad)=>{
-    const {email, password} = payLoad;
+const longinUser = async (payLoad: AuthPayLoad) => {
+    const { email, password } = payLoad;
 
     const user = await prisma.user.findUniqueOrThrow({
-        where : {
+        where: {
             email
         }
     })
 
     const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if(!isPasswordMatch){
+    if (!isPasswordMatch) {
         throw new Error("Password is Incorrect!");
     }
 
-    return user;
+    //? password is matched now we will generate an access token
+    const jwtPayLoad = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+    }
+
+    console.log(typeof config.jwt_access_token_expiration);
+    console.log(typeof config.jwt_refresh_token_expiration);
+    console.log(config.jwt_access_token_expiration);
+    console.log(config.jwt_refresh_token_expiration);
+
+    const accessToken = jwt.sign(jwtPayLoad, config.jwt_secret,
+        {
+            expiresIn: config.jwt_access_token_expiration
+        } as SignOptions
+    );
+
+    //? generating the refresh token
+    const refreshToken = jwt.sign(jwtPayLoad, config.jwt_refresh_secret,
+        {
+            expiresIn: config.jwt_refresh_token_expiration
+        } as SignOptions
+    )
+
+    return {
+        accessToken,
+        refreshToken
+    };
 }
 
 const authServices = {
