5425988c01715378cf6a29fccb95e70a5bd8020e
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-06-29 22:53:47 +0600

    optimized searching sorting paginating

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -7,8 +7,9 @@ import { Role } from "../../../generated/prisma/enums";
 
 //? getting all the posts
 const getAllPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
     const query = req.query;
-    
+
     const allPosts = await postService.getAllPosts(query);
 
     return sendResponse2(res, {
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,70 +1,110 @@
+import { SrvRecord } from "node:dns";
 import { CommentStaus, PostStatus } from "../../../generated/prisma/enums";
 import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 import { ICreatePostPayLoad, IPostQuery, IUpdatePostPayLoad } from "./post.interface";
 
 
-const getAllPosts = async (query : IPostQuery) => {
+const getAllPosts = async (query: IPostQuery) => {
 
     const limit = query.limit ? Number(query.limit) : 10;
-    const page = query.page ? Number(query.page) : 1 ;
+    const page = query.page ? Number(query.page) : 1;
     const skip = (page - 1) * limit;
 
     const sortBy = query.sortBy ? query.sortBy : 'createdAt';
     const sortOrder = query.sortOrder ? query.sortOrder : "asc";
 
-    const result = await prisma.post.findMany({
-        where : {
-            AND : [
 
-                //? 1
-               query.title ? {
-                title : query.title
-               } : {},
+    //? we found some issues here
+    const tags = query.tags ? JSON.parse(query.tags as string) : null;
+    const tagsArray = Array.isArray(tags) ? tags : [];
 
-               //? 2
-               query.content ? {
-                content : query.content
-               } : {},
 
-               //? 3
-               query.searchTerm ? {
-                OR : [
-                    {
-                        title : {
-                            contains : query.searchTerm,
-                            mode : 'insensitive'
-                        }
-                    },
-                    {
-                        content : {
-                            contains : query.searchTerm,
-                            mode : 'insensitive'
-                        }
+    const andConditions: PostWhereInput[] = []
+
+    if (query.searchTerm) {
+        andConditions.push({
+            OR: [
+                {
+                    title: {
+                        contains: query.searchTerm,
+                        mode: 'insensitive'
                     }
-                ]
-               } : {},
-            ]
-        },
-        include: {
-            author: {
-                omit: {
-                    password: true
+                },
+                {
+                    content: {
+                        contains: query.searchTerm,
+                        mode: 'insensitive'
+                    }
                 }
+            ]
+        });
+    }
+    if (query.title) {
+        andConditions.push({
+            title: query.title
+        })
+    }
+
+    if (query.content) {
+        andConditions.push({
+            content: query.content
+        })
+    }
+
+    if (query.authorId) {
+        andConditions.push({
+            authorId: query.authorId
+        })
+    }
+
+    if (query.isFeatured) {
+        andConditions.push({
+            isFeatured: Boolean(query.isFeatured)
+        })
+    }
+
+    if (query.tags) {
+        andConditions.push({
+            tags: {
+                hasSome: tagsArray
+            }
+        })
+    }
+
+    if (query.status) {
+        andConditions.push({
+            status: query.status
+        })
+    }
+
+
+    {
+
+        const result = await prisma.post.findMany({
+            where: {
+                AND: andConditions
             },
-            comments: true
-        },
+            include: {
+                author: {
+                    omit: {
+                        password: true
+                    }
+                },
+                comments: true
+            },
 
-        //? dynamic pagination
-        take : limit,
-        skip : skip,
+            //? dynamic pagination
+            take: limit,
+            skip: skip,
 
-        orderBy : {
-            [sortBy] : sortOrder
-        }
-    });
-    return result;
-};
+            orderBy: {
+                [sortBy]: sortOrder
+            }
+        });
+        return result;
+    };
+}
 
 const getPostStats = async () => {
 
