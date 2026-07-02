a6f4457d9ef4b0f6e31da99a331d1f9e1a33c597
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-02 17:08:12 +0600

    checking if the user exists who is trying to pay

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,7 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../../utility/catchAsync";
+import { subsbscriptionService } from "./subscription.service";
+import sendResponse2 from "../../utility/sendResponse2";
+import status from "http-status";
 
 const createCheckOutSession = catchAsync(async (req : Request, res : Response, next : NextFunction) => {
+
+    const userId = req.user?.id as string;
+
+    const result = await subsbscriptionService.createCheckhOutSession(userId);
+
+    return sendResponse2(res, {
+        success : true,
+        statusCode : status.OK,
+        message : "Checkout Completely successsfull",
+        data : result
+    })
 
 })
 
--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,5 +1,29 @@
-const createCheckhOutSession = () =>{
+import { prisma } from "../../lib/prisma";
 
+const createCheckhOutSession = async(userId : string) =>{
+
+    const transactionResult = await prisma.$transaction(async(tx)=>{
+
+        //? checking if the user exists or not
+        const user = await tx.user.findUnique({
+            where : {
+                id : userId
+            },
+            include : {
+                subscription : true
+            }
+        })
+
+        if(!user){
+            throw new Error("User not found");
+        }
+
+        // let stripeCustomerId = user.subscription. 
+
+
+    })
+
+    return transactionResult;
 };
 
 export const subsbscriptionService = {
