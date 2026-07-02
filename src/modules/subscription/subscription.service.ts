7072581a9037e341915c90ec4fd7a591076eb8d4
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-07-02 21:33:47 +0600

    added two new function dor the stripe checkout session and storing user information

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,3 +1,4 @@
+import Stripe from "stripe";
 import config from "../../config/dotenv";
 import { prisma } from "../../lib/prisma";
 import stripe from "../../lib/strip";
@@ -70,23 +71,78 @@ const weebHookHandler = async (payLoad: Buffer, signature: string) => {
 
     switch (event.type) {
         case 'checkout.session.completed':
-        //? occures when a payment session is completed successfully
-            const paymentObject = event.data.object;
+            //? occures when a payment session is completed successfully
+            const session: Stripe.Checkout.Session = event.data.object;
+
+            handleCheckOutSessionComplete(session);
+
             break;
         case 'customer.subscription.updated':
-        //? occures when a subscription is updated from one plan to another or free to premium 
-           
+            //? occures when a subscription is updated from one plan to another or free to premium 
+
             break;
         case 'customer.subscription.deleted':
             //? occures when a customer subscription ends
             const paymentMethod = event.data.object;
-          
+
             break;
         default:
-   
+
             console.log(`No event matched - ${event.type}.`);
             break;
     }
+}
+
+const getPeriodEndTime = (payLoad: Stripe.Subscription) => {
+    const currentPeriodEndInMiliSeconds = payLoad.items.data[0]?.current_period_end!;
+
+    const cureentPeriodEnds = new Date(currentPeriodEndInMiliSeconds * 1000);
+
+    return cureentPeriodEnds;
+
+}
+
+const handleCheckOutSessionComplete = async (session: Stripe.Checkout.Session) => {
+    const userId = session.metadata?.userId as string;
+
+    //? getting the stripe customer id
+    const stripeCustomerId = session.customer as string;
+
+    //? getting the stripe subscriptionId
+    const stripeSubscriptionId = session.subscription as string;
+
+    if (!userId || !stripeSubscriptionId || !stripeCustomerId) {
+        throw new Error("Webhook failed!");
+    }
+
+    //? stripe subscription information getting
+    const subcriptionInformation = await stripe.subscriptions.retrieve(stripeSubscriptionId as string);
+
+    // console.log("subcription information",subcriptionInformation.items.data[0]);
+
+    //? getting the subscription end time
+
+    const cureentPeriodEnds = getPeriodEndTime(subcriptionInformation);
+
+    //? inserting into the database if not exist if exist updating it
+
+    await prisma.subscription.upsert({
+        where: {
+            userId
+        },
+        create: {
+            userId,
+            stripeCustomerId,
+            stripeSubscriptionId: stripeSubscriptionId,
+            current_period_end: cureentPeriodEnds
+        },
+        update: {
+            stripeCustomerId,
+            stripeSubscriptionId: stripeSubscriptionId,
+            current_period_end: cureentPeriodEnds
+        }
+    })
+
 }
 
 export const subsbscriptionService = {
